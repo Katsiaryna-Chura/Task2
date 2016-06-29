94cb79ccd6a3c4344b07adf3be6c478189aa3ddb
Katsiaryna Chura <katsiaryna_chura@epam.com>
2016-06-29 11:33:51 +0300

    add second part of task2

---
+++ Task 2_2/Task 2_2/DataHandler.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_2
+{
+    public class DataHandler
+    {
+        List<int> intList;
+        List<double> doubleList;
+        List<string> stringList;
+
+        public DataHandler(List<string> list)
+        {
+            int i;
+            double d;
+            intList = new List<int>();
+            doubleList = new List<double>();
+            stringList = new List<string>();
+            foreach (var listItem in list)
+            {
+                if (Int32.TryParse(listItem, out i))
+                {
+                    intList.Add(i);
+                }
+                else if (Double.TryParse(listItem.Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out d))
+                {
+                    doubleList.Add(d);
+                }
+                else
+                {
+                    stringList.Add(listItem);
+                }
+            }
+        }
+
+        public int GetNumberOfIntegers()
+        {
+            return intList.Count;
+        }
+
+        public List<int> GetListOfIntegers()
+        {
+            return intList;
+        }
+
+        public double GetIntegersAverage()
+        {
+            if (intList.Count == 0)
+            {
+                return 0;
+            }
+            return intList.Average();
+        }
+
+        public int GetNumberOfDoubles()
+        {
+            return doubleList.Count;
+        }
+
+        public List<double> GetListOfDoubles()
+        {
+            return doubleList;
+        }
+
+        public double GetDoublesAverage()
+        {
+            if (doubleList.Count == 0)
+            {
+                return 0;
+            }
+            return doubleList.Average();
+        }
+
+        public List<string> GetSortedStrings()
+        {
+            return stringList.OrderBy(a => a.Length).ThenBy(a => a).ToList();
+        }
+    }
+}
---
+++ Task 2_2/Task 2_2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataHandler dataHandler;
+            List<string> list = new List<string>();
+            string str;
+            do
+            {
+                Console.WriteLine("Enter string (or 'stop' to stop entering new strings):");
+                str = Console.ReadLine();
+                if (str != "stop")
+                {
+                    list.Add(str);
+                }
+            } while (str != "stop");
+            dataHandler = new DataHandler(list);
+            Console.WriteLine($"Number of integers: {dataHandler.GetNumberOfIntegers()}");  
+            WriteIntegersWithAverage(dataHandler.GetListOfIntegers(), dataHandler.GetIntegersAverage());
+            Console.WriteLine($"Number of doubles: {dataHandler.GetNumberOfDoubles()}"); 
+            WriteDoublesWithAverage(dataHandler.GetListOfDoubles(), dataHandler.GetDoublesAverage());
+            WriteSortedStrings(dataHandler.GetSortedStrings());
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        public static void WriteIntegersWithAverage(List<int> intList, double avg)
+        {
+            if (intList.Count == 0)
+            {
+                return;
+            }
+            foreach (int i in intList)
+            {
+                Console.WriteLine($"{i,17}");
+            }
+            Console.WriteLine($"average = {avg,7:0.00}");
+        }
+
+        public static void WriteDoublesWithAverage(List<double> doubleList, double avg)
+        {
+            if (doubleList.Count == 0)
+            {
+                return;
+            }
+            foreach (var d in doubleList)
+            {
+                Console.WriteLine($"{d,17:0.00}");
+            }
+            Console.WriteLine($"average = {avg,7:0.00}");
+        }
+
+        public static void WriteSortedStrings(List<string> sortedList)
+        {
+            if (sortedList.Count == 0)
+            {
+                Console.WriteLine("There are no strings that are not numbers.");
+                return;
+            }
+            Console.WriteLine("Sorted strings:");
+            foreach (var s in sortedList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
